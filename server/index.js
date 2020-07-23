8f1f83a3064ea40d818066e6d62a01902b12c057
Howard Wang <dr.howardwang@gmail.com>
2020-07-23 07:43:24 -0700

    id generation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ var helmet = require('helmet');
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var validUrl = require('valid-url');
+var schemaValidator = require('schema-validator');
+var yup = require('yup');
+var { nanoid } = require('nanoid');
 
 
 var app = express();
@@ -28,14 +32,40 @@ app.get('/:id', (req, res) => {
 	});
 });
 
-app.post('/create', (req, res) => {
+const schema = yup.object().shape({
+	id: yup.string().trim().matches(/[a-zA-Z0-9_\-]/).max(6),
+	url: yup.string().trim().url().required()
+});
 
-	console.log(req.body);
-	res.json({
-		message: 'url creation',
-		id: req.body.id,
-		url: req.body.url,
-	});
+app.post('/create', async (req, res, next) => {
+	// console.log(req.body);
+	// const id = req.body.id;
+	// const url = req.body.url;
+	let { id, url } = req.body;
+
+	try {
+		await schema.validate({
+			id: id,
+			url: url
+		});
+
+		//generate id if none found
+		if(!id) id = nanoid(6);
+
+
+		
+
+		
+		res.json({
+			message: 'url creation',
+			id: id,
+			url: url,
+		});
+
+
+	} catch(err) {
+		next(err);
+	}
 });
 
 const PORT = process.env.PORT || 6969;
